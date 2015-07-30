90b9d26d5f90e148761910b554947782ef4c60b3
Gustavo Franckevicius <gus.fgm@hotmail.com>
2015-07-30 15:47:59 -0300

    Arquivos que estavam faltando

---
+++ Projeto Unity MusiKO/Assets/Scripts/Controladores/ControladorQuests.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorQuests {
+
+	[System.Serializable]
+	public struct questChain{
+		public string chainName;
+		public questSteps[] chainSteps;
+
+		[HideInInspector]
+		public bool chainComplete;
+		[HideInInspector]
+		public bool chainActive;
+		[HideInInspector]
+		public int chainStep;
+
+		public questChain(int chainStepNumber){
+			chainName = "";
+			chainSteps = new questSteps[chainStepNumber];
+			chainComplete = false;
+			chainActive = false;
+			chainStep = 0;
+		}
+	}
+
+	[System.Serializable]
+	public struct questSteps{
+		public string stepName;
+		public int stepQtdeDeliver;
+		public Transform stepTarget;
+
+		[HideInInspector]
+		public bool stepComplete;
+		[HideInInspector]
+		public bool stepActive;
+
+		public questSteps(string stepCurrName){
+			stepName = stepCurrName;
+			stepQtdeDeliver = 0;
+			stepTarget = null;
+			stepComplete = false;
+			stepActive = false;
+		}
+	}
+
+}
---
+++ Projeto Unity MusiKO/Assets/Scripts/Triggers/Quests.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quests : MonoBehaviour {
+
+	private GameMasterFora gameMaster;
+	private ControladorQuests.questChain[] presentChains;
+
+	public bool loadNewMap;
+	public string newMapName;
+
+	// Use this for initialization
+	void Start () {
+		gameMaster = FindObjectOfType<GameMasterFora> ();
+		presentChains = gameMaster.presentChains;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			for (int i=0;i<presentChains.Length;i++){
+				if(!presentChains[i].chainActive)
+					continue;
+
+				for(int j=0;j<presentChains[i].chainSteps.Length;j++){
+					if(!presentChains[i].chainSteps[j].stepActive)
+						continue;
+
+					/*if (--pseudo codigo--player.numeroDeItensQuest--final pseudo codigo-- >= presentChains[i].chainSteps[j].stepQtdeDeliver){
+						advanceQuest(i,j,loadNewMap);
+					}*/
+
+
+					if (transform.position == presentChains[i].chainSteps[j].stepTarget.position){
+						advanceQuest(i,j,loadNewMap);
+					}
+
+				}
+			}
+		}
+	}
+
+	void advanceQuest(int chain, int step, bool shouldLoad){
+		presentChains[chain].chainSteps[step].stepComplete = true;
+		presentChains[chain].chainSteps[step].stepActive = false;
+		if (step < presentChains[chain].chainSteps.Length -1){
+			presentChains[chain].chainSteps[step+1].stepActive = true;
+			presentChains[chain].chainStep++;
+			print ("Quest Step Complete");
+		}
+		else{
+			presentChains[chain].chainComplete = true;
+			presentChains[chain].chainActive = false;
+			print ("Quest Chain Complete");
+		}
+
+		if (shouldLoad) {
+			GameObject.FindGameObjectWithTag("Player").GetComponent<ForaCombate>().saveStatus();
+			Application.LoadLevel (newMapName); 
+		}
+	}}
